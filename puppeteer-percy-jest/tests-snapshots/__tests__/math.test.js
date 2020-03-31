ef5fa644023e5d286f9deedc912edf32397036e0
rijuvijayan <rijuvijayan@gmail.com>
2020-03-31 22:47:10 +1300

    fixed

--- puppeteer-percy-jest/tests-snapshots/__tests__/math.test.js
+++ puppeteer-percy-jest/tests-snapshots/__tests__/math.test.js
@@ -1,7 +1,7 @@
 const sum = require('../../math');
 
 describe('Sum of two numbers', () => {
-  test('should be 10', () => {
-    expect(sum(5, 5)).toBe(11);
+  test('should be 100', () => {
+    expect(sum(50, 50)).toBe(100);
   });
 });
