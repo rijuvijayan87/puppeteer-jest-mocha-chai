64efa96c9ec2d1e69a50d21a2d6061c5257fc2f2
rijuvijayan <rijuvijayan@gmail.com>
2020-03-31 22:44:54 +1300

    introducing a failure

--- puppeteer-percy-jest/tests-snapshots/__tests__/math.test.js
+++ puppeteer-percy-jest/tests-snapshots/__tests__/math.test.js
@@ -2,6 +2,6 @@ const sum = require('../../math');
 
 describe('Sum of two numbers', () => {
   test('should be 10', () => {
-    expect(sum(5, 5)).toBe(10);
+    expect(sum(5, 5)).toBe(11);
   });
 });
