025bd56187b917f781f61b390402136e2ae7a091
rijuvijayan <rijuvijayan@gmail.com>
2020-04-01 22:25:50 +1300

    feedback page done

--- puppeteer-jest/tests/example.test.js
+++ puppeteer-jest/tests/example.test.js
@@ -1,17 +1,20 @@
 import HomePage from '../pages/HomePage';
 import LoginPage from '../pages/LoginPage';
+import FeedbackPage from '../pages/FeedbackPage';
 import TopBar from '../pages/components/TopBar';
 
 describe('Example', () => {
   let homepage;
   let topBar;
   let loginPage;
+  let feedbackPage;
 
   beforeAll(async () => {
     jest.setTimeout(15000);
     homepage = new HomePage();
     topBar = new TopBar();
     loginPage = new LoginPage();
+    feedbackPage = new FeedbackPage();
   });
 
   it('home page should work', async () => {
@@ -26,5 +29,16 @@ describe('Example', () => {
   it('Try to login', async () => {
     await loginPage.isLoginFormDisplayed();
     await loginPage.login('username', 'password');
+  });
+
+  it('Feedback page', async () => {
+    await feedbackPage.visit();
+    await feedbackPage.isFeedbackformDisplayed();
+    await feedbackPage.submitFeedback(
+      'Test',
+      'test@gmail.com',
+      'subject',
+      'comment'
+    );
   });
 });
