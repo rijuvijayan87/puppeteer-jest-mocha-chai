9af2ccb41eabfc3d501e2dc68790472dd0daf39b
rijuvijayan <rijuvijayan@gmail.com>
2020-04-01 23:05:00 +1300

    obscate username, password and timeout onto a config file

--- puppeteer-jest/tests/end-to-end.test.js
+++ puppeteer-jest/tests/end-to-end.test.js
@@ -3,32 +3,34 @@ import LoginPage from '../pages/LoginPage';
 import FeedbackPage from '../pages/FeedbackPage';
 import TopBar from '../pages/components/TopBar';
 
-describe('Example', () => {
-  let homepage;
+import { username, password, timeout } from '../tests/config';
+
+describe('End-To-End Test', () => {
+  let homePage;
   let topBar;
   let loginPage;
   let feedbackPage;
 
   beforeAll(async () => {
-    jest.setTimeout(15000);
-    homepage = new HomePage();
+    jest.setTimeout(timeout);
+    homePage = new HomePage();
     topBar = new TopBar();
     loginPage = new LoginPage();
     feedbackPage = new FeedbackPage();
   });
 
   it('home page should work', async () => {
-    await homepage.visit();
+    await homePage.visit();
   });
 
   it('navbar should be displayed', async () => {
-    await homepage.isNavBarDisplayed();
+    await homePage.isNavBarDisplayed();
     await topBar.isTopBarDisplayed();
   });
 
   it('Try to login', async () => {
     await loginPage.isLoginFormDisplayed();
-    await loginPage.login('username', 'password');
+    await loginPage.login(username, password);
   });
 
   it('Feedback page', async () => {
